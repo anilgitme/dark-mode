3ac1f65a4558147c1cc269447c278b1f4f8ab815
Anil_Ramcharran <anil.gitme@gmail.com>
2021-01-19 19:49:36 -0500

    refactored the app to use my hooks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
+// import {useDarkMode} from './hooks/useDarkMode';
 
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
@@ -9,7 +10,7 @@ import "./styles.scss";
 
 const App = () => {
   const [coinData, setCoinData] = useState([]);
-  const [darkMode, setDarkMode] = useState(false);
+  // const [darkMode, setDarkMode] = useState(false);
 
   useEffect(() => {
     axios
@@ -20,8 +21,8 @@ const App = () => {
       .catch(err => console.log(err));
   }, []);
   return (
-    <div className={darkMode ? "dark-mode App" : "App"}>
-      <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
+    <div className={'' ? "dark-mode App" : "App"}>
+      <Navbar />
       <Charts coinData={coinData} />
     </div>
   );
